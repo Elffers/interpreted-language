a38ac009244522f90b3fe339fa610d317287e2d8
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-17 01:20:42 -0800

    add test file

--- anaconda.rb
+++ anaconda.rb
@@ -1,4 +1,4 @@
-lines = File.readlines("program.an")
+lines = File.readlines("test.an")
 
 assignment_hash = {}
 lines.each do |line|
@@ -22,24 +22,24 @@ assignment_hash.each do |k, v|
 end
 
 #Parse value of assigment hash into array if contains binary operator
-  assignment_hash.each do |k,v|
-    if v.is_a? String
-      operators = %w[+ - * /]
-      for x in operators
-        if v.include?(x)
-          assignment_hash[k] = v.partition(" #{x} ")
-        end
+assignment_hash.each do |k,v|
+  if v.is_a? String
+    operators = %w[+ - * /]
+    for x in operators
+      if v.include?(x)
+        assignment_hash[k] = v.partition(" #{x} ")
       end
-      # evaluates the variables in the expression
-      assignment_hash[k] = v.map do |variable|
-        if assignment_hash[variable]
-          variable = assignment_hash[variable] 
-        else
-          variable
-        end
-      end  
     end
+    # evaluates the variables in the expression, which should be an array
+    # assignment_hash[k] = v.map do |variable|
+    #   if assignment_hash[variable]
+    #     variable = assignment_hash[variable] 
+    #   else
+    #     variable
+    #   end
+    # end  
   end
+end
 
 # evaluate variables in expressions
 assignment_hash.each do |k, v|
