4d5707c992de68f6c2c1de62fd3708bce49b7e43
Jon Bauman <jongithub@shumi.fake>
2014-02-17 13:26:47 -0800

    Add a file with some very basic unit tests for anaconda.

---
+++ test_anaconda.rb
@@ -0,0 +1,26 @@
+require_relative 'anaconda'
+require 'test/unit'
+
+def interpret_string(program)
+  return interpret(program.split("\n"))
+end
+
+class TestAnaconda < Test::Unit::TestCase
+  def test_print
+    program =<<-END
+x = 1
+print x
+    END
+    return_value = interpret_string(program)
+    assert_equal(1, return_value)
+  end
+  
+  def test_print_literal
+    program =<<-END
+print 1
+    END
+    puts program
+    return_value = interpret_string(program)
+    assert_equal(1, return_value)
+  end
+end
