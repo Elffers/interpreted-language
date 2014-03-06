867f845d54f81a02c7ab3e9beef8d2c53d8359bf
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-05 22:43:13 -0800

    add tests for all operators, non binary expressions

--- test_anaconda.rb
+++ test_anaconda.rb
@@ -35,7 +35,76 @@ print y
     assert_equal([1, 2], return_value)
   end
 
-  # test all the operators
+  def test_print_expression
+    program=<<-END
+x = 1
+y = 3
+print y-x
+    END
+    return_value = interpret_string(program)
+    assert_equal([2], return_value)
+  end
+
+  def test_subtract
+    program=<<-END
+x = 1
+y = 5-x
+print y
+    END
+    return_value = interpret_string(program)
+    assert_equal([4], return_value)
+  end
+
+  def test_multiply
+    program=<<-END
+x = 2
+y = 5 * x 
+print y
+    END
+    return_value = interpret_string(program)
+    assert_equal([10], return_value)
+  end
+
+  def test_divide
+    program=<<-END
+x = 2
+y = 5 / x
+print y
+    END
+    return_value = interpret_string(program)
+    assert_equal([2], return_value)
+  end
+
+#   def test_undefined_variable
+#     program=<<-END
+# x = 2
+# y = 5 + z
+# print y
+#     END
+#     return_value = interpret_string(program)
+#     assert_equal([2], return_value)
+#   end
+
+  def test_non_binary_add1
+    program=<<-END
+x = 2
+y = 5 + x + 3
+print y
+    END
+    return_value = interpret_string(program)
+    assert_equal([10], return_value)
+  end
+
+  def test_non_binary_add2
+    program=<<-END
+x = 2
+y = 5 + 3 + x
+print y
+    END
+    return_value = interpret_string(program)
+    assert_equal([10], return_value)
+  end
+  
   # test variable that hasn't been defined
   # test non-binary expressions
 end
