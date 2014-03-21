a7fa58c2d960f861c34a11188d659c57e81fb7ee
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-20 23:26:17 -0700

    more descriptive names

--- anaconda.rb
+++ anaconda.rb
@@ -1,9 +1,11 @@
+# Script to interpret a basic theoretical language ("anaconda") that uses only simple variable assignments, binary operators and print statements
+
 def interpret(lines)
   assignment_hash = {}
   output = []
   lines.each do |line|
     if line.include? "="
-      lhs, rhs = line.split(" = ").map {|char| char.strip}
+      lhs, rhs = line.split("=").map {|char| char.strip}
       assignment_hash[lhs] = evaluate(rhs, assignment_hash)
     elsif line.include? "print"
       m = line.match(/print /)
@@ -20,10 +22,10 @@ def evaluate(expression, assignment_hash)
   elsif expression =~ /^\w+$/
     raise Error unless assignment_hash.has_key?(expression)
       assignment_hash[expression] 
-  elsif md = expression.match(/[-+*\/]/)
-    operator = md.to_s 
+  elsif matchdata = expression.match(/[-+*\/]/)
+    operator = matchdata.to_s 
     operands = expression.split(operator)
-    operand1, operand2 = operands.map {|o| evaluate(o, assignment_hash)}
+    operand1, operand2 = operands.map {|operand| evaluate(operand, assignment_hash)}
     case operator
       when "+"
         operand1 + operand2
